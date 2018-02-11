50ed20f34e8629d95c5e0637cad08e2ce689d12e
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-02-10 16:57:08 -0800

    allows users to register with a username instead of an email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,16 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :primary_teams,   class_name: 'Team', foreign_key: 'captain_id'
   has_many :secondary_teams, class_name: 'Team', foreign_key: 'player_id'
 
-  before_save :downcase_email
+  VALID_HANDLE_REGEX = /\A[a-z\d]*\Z/i
 
-  has_secure_password
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :handle, presence: true, length: { maximum: 15 }, uniqueness: true
-  validates :email,  presence: true, length: { maximum: 255 },
-                     format: { with: VALID_EMAIL_REGEX },
+  validates :handle, presence: true, length: { maximum: 15 },
+                     format: { with: VALID_HANDLE_REGEX },
                      uniqueness: { case_sensitive: false }
 
   def self.from_token_request(request)
     handle = request.params['auth'] && request.params['auth']['handle']
     find_by(handle: handle)
-  end
-
-  private
-
-  def downcase_email
-    self.email = email.downcase
   end
 end
