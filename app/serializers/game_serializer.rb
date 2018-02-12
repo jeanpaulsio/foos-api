b78e66b9b146f94000d158baf24f66d21c23057e
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-02-11 20:00:56 -0800

    returns winning and losing team names when fetching all games

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,11 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :winning_team_id, :losing_team_id
+  attributes :id, :winning_team, :losing_team
+
+  def winning_team
+    "#{object.winning_team.captain.handle} and #{object.winning_team.player.handle}"
+  end
+
+  def losing_team
+    "#{object.losing_team.captain.handle} and #{object.losing_team.player.handle}"
+  end
 end
